33e832a1f3bbfb90ddd6ef5b94691ee575e1d37e
DJDavid98 <djdavid98@protonmail.com>
2021-09-17 21:47:25 +0200

    Make Mozilla Add-on reviewers happy (hopefully)

--- src/vendor/preact.ts
+++ src/vendor/preact.ts
@@ -1,33 +1,17 @@
+/**
+ * @fileOverview This file is used to consume the preact umd bundle in a type-safe way. All files that need preact
+ *               must import it from here to avoid runtime errors.
+ */
+
 /* eslint-disable no-restricted-imports */
-import type { ComponentClass, h as pH, render as pRender } from 'preact';
-import type {
-  useCallback as pUseCallback,
-  useEffect as pUseEffect,
-  useRef as pUseRef,
-  useState as pUseState,
-  useMemo as pUseMemo,
-} from 'preact/hooks';
-
+import type * as PreactUmdGlobal from 'preact';
+import type * as PreactHooksUmdGlobal from 'preact/hooks';
 /* eslint-enable no-restricted-imports */
-
-export interface PreactUmdGlobal {
-  h: typeof pH;
-  Fragment: ComponentClass<{ key?: unknown }>;
-  render: typeof pRender;
-}
-
-export interface PreactHooksUmdGlobal {
-  useCallback: typeof pUseCallback;
-  useEffect: typeof pUseEffect;
-  useRef: typeof pUseRef;
-  useState: typeof pUseState;
-  useMemo: typeof pUseMemo;
-}
 
 declare global {
   interface Window {
-    preact: PreactUmdGlobal;
-    preactHooks: PreactHooksUmdGlobal;
+    preact: typeof PreactUmdGlobal;
+    preactHooks: typeof PreactHooksUmdGlobal;
   }
 }
 
